707a1f06a4f60256cf32bbc848cb7554deb1ae27
seriousManual <seriousstuff@gmx.de>
2016-06-28 06:51:45 +0200

    reformat

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ var expect = require('chai').expect
 
 var allcombinations = require('./index')
 
-expect(Array.from(allcombinations([1,2,3]))).to.deep.equal([
+expect(Array.from(allcombinations([1, 2, 3]))).to.deep.equal([
     [1, 2, 3],
     [1, 3, 2],
     [2, 1, 3],
@@ -10,3 +10,5 @@ expect(Array.from(allcombinations([1,2,3]))).to.deep.equal([
     [3, 1, 2],
     [3, 2, 1]
 ])
+
+console.log('kthxbye')
